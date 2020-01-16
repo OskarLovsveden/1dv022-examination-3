a4afe2190d496f91deaaa1b1fd5c2772e9eb9f4a
OskarLovsveden <ol222hf@student.lnu.se>
2020-01-16 14:45:04 +0100

    window templates moved to another js file

--- src/js/desktop-ui/desktop-window.js
+++ src/js/desktop-ui/desktop-window.js
@@ -1,80 +1,17 @@
-const desktopWindowTemplate = document.createElement('template')
-desktopWindowTemplate.innerHTML = `
-<style>
-:host {
-  min-height: 410px;
-  min-width: 300px;
-  position: absolute;
-  background-color: #f5f5f5;
-  border: 2px solid lightgrey;
-  border-radius: 5px 5px 0 0;
-  overflow: hidden;
-  font-family: Verdana, sans-serif;
-  color: #000000;
-  text-decoration: none;
-  font-style: normal;
-  font-variant: normal;
-  text-transform: none;
-}
-:host(:focus) {
-  outline: none;
-}
-header {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 45px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgb(125, 125, 125, 0.1);
-}
-header img {
-  width: 45px;
-  height: 45px;
-  margin: 0;
-  padding: 0;
-}
-header p:hover {
-  cursor: default;
-}
-#contentWindow {
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  width: 100%;
-  height: 90%;
-}
-#closeBtn {
-  position: absolute;
-  top: 0;
-  left: 0;
-  -webkit-transform: scale(1);
-  transform: scale(1);
-  -webkit-transition: .3s ease-in-out;
-  transition: .3s ease-in-out;
-}
-#closeBtn:hover {
-  -webkit-transform: scale(1.3);
-  transform: scale(1.3);
-}
-#appIcon {
-  position: absolute;
-  top: 0;
-  right: 0;
-}
-</style>
-<header id="headerWindow">
-<img id="closeBtn" src="./image/x.svg" alt="An X">
-<p id="appName"></p>
-<img id="appIcon" src="" alt="current app">
-</header>
-<div id="contentWindow">
-</div>
-`
+import { desktopWindowTemplate } from './desktop-template.js'
 
+/**
+ * Web-component for a desktop window
+ *
+ * @export
+ * @class DesktopWindow
+ * @extends {window.HTMLElement}
+ */
 export default class DesktopWindow extends window.HTMLElement {
+  /**
+   * Creates an instance of DesktopWindow.
+   * @memberof DesktopWindow
+   */
   constructor () {
     super()
 
@@ -97,6 +34,11 @@ export default class DesktopWindow extends window.HTMLElement {
     this.isDown = false
   }
 
+  /**
+   * Opens the application corresponding to this._name.
+   * Adds event listener for mousedown, mouseup and mousemove to move the windows.
+   * Adds event listener for click to close the windows.
+   */
   connectedCallback () {
     const newApp = document.createElement(this._name)
     this._contentWindow.appendChild(newApp)
@@ -135,10 +77,19 @@ export default class DesktopWindow extends window.HTMLElement {
     })
   }
 
+  /** Tells the web-component what attributes to watch for changes */
   static get observedAttributes () {
     return ['name', 'src', 'id']
   }
 
+  /**
+   * When an attribute is updated, check if the value is different
+   *
+   * @param {string} attributes - The attribute that changed
+   * @param {string} oldValue - The old value
+   * @param {string} newValue - The new value
+   * @memberof DesktopWindow
+   */
   attributeChangedCallback (attributes, oldValue, newValue) {
     switch (attributes) {
       case 'name':
